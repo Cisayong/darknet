1ee1f6a2b8044bfd9fd7796d6a808225c5311410
Cisayong <55373421+Cisayong@users.noreply.github.com>
2019-09-18 20:43:33 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo -e " Proses Penginstalan "
+sleep 2
+clear
+pkg install figlet
+pkg install toilet
+pkg inatall lolcat
+pkg install Ruby
+pkg install gem
+gem install lolcat
+pip2 install requests
+pip2 install mechanize
+pip2 install termocolor
+clear
+figlet Selesai | lolcat
+cd darknet
+python2 contoh.pyc
